4e47cae2f7e3c02ebd477f0073fd97b42a0a6887
yuta17 <usgitan@gmail.com>
2022-02-02 16:30:12 +0900

    Add greet method

--- test/greeter_test.js
+++ test/greeter_test.js
@@ -4,5 +4,14 @@ contract("Greeter", () => {
   it("has been deployed successfully", async () => {
     const greeter = await GreeterContract.deployed();
     assert(greeter, "contract was not deployed");
+  });
+
+  describe("greet()", () => {
+    it("returns 'Hello, world!'", async () => {
+      const greeter = await GreeterContract.deployed();
+      const expected = "Hello, World!";
+      const actual = await greeter.greet();
+      assert.equal(actual, expected, "greeted with 'Hello, World!'");
+    })
   })
 })
